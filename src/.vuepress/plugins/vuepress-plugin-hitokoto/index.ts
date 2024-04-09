8b7bf65352704e67eafafe92920fbf87bc750ad0
tmh <t13219006372@163.com>
2024-04-09 18:12:16 +0800

    feat: first commit

---
+++ src/.vuepress/plugins/vuepress-plugin-hitokoto/index.ts
@@ -0,0 +1,19 @@
+import { PluginFunction } from 'vuepress'
+import { getDirname, path } from 'vuepress/utils'
+
+const __dirname = getDirname(import.meta.url)
+interface HitokotoOption {}
+const hitokotoPlugin = (options?: HitokotoOption): PluginFunction => {
+  return app => {
+    return {
+      name: 'vuepress-plugin-hitokoto',
+      define: {
+        hitokotoOptions: options
+      },
+      multiple: false,
+      clientConfigFile: path.resolve(__dirname, './client/hitokoto-client-config.ts')
+    }
+  }
+}
+export { hitokotoPlugin }
+export type { HitokotoOption }
---
+++ src/.vuepress/theme/components/props.ts
@@ -0,0 +1,5 @@
+import { useArticleInfo } from '@theme-hope/modules/blog/composables/index'
+// @ts-ignore
+import { __VLS_props } from './NewsItem.vue'
+
+export const props = __VLS_props as typeof useArticleInfo
