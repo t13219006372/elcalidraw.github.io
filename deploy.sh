82c8add9c4c566b2f28120a2f56b7e1385bcbd69
tmh <t13219006372@163.com>
2024-04-13 22:06:38 +0800

    fix(ci): add clean operate for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ echo "deploy front env-----------------------"
 echo "build online file"
 pnpm build
 
+echo "clean dist"
+ssh root@singlequote.cn "rm -rf /etc/nginx/www/blog/dist/*"
+echo "clean done"
+
 echo "upload to server"
 scp -r ./src/.vuepress/dist/* root@43.134.15.128:/etc/nginx/www/blog/dist/
 echo "done"
